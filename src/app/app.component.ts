eafcb6916d67d4cf8ce6442424153c3f5457d195
Ranjitha <ranjitha.amittisoft.com>
2019-06-21 19:09:22 +0530

    form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,33 @@ export class AppComponent implements OnInit{
 
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsername = ['chris','anna'];
 
   ngOnInit(){
     this.signupForm = new FormGroup({
       'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('male')
+      'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
   }
   onSubmit(){
     console.log(this.signupForm);
+  }
+  onAddHobby(){
+    (<FormArray>this.signupForm.get('hobbies')).push(new FormControl(null, Validators.required));
+  }
+  forbiddenEmails(control: FormControl):Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout( ()=>{
+        if(control.value === 'test@gmail.com'){
+          resolve({'emailIsForbidden' : true});
+        }
+        else{
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise
   }
 }
